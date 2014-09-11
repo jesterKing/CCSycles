c52ffaac782ed8a27c514e4d3ac9abc600f4acc1
Nathan Letwory <nathan@mcneel.com>
2014-09-11 10:48:09 +0300

    First usable iteration of the exporter is now ready.
    * Add documentation
    * Reorganise a bit
    * Rename functions to be more clear
    * Remove unused/commented out code
    * Exported code formatting
    * Shader code can now be found in a text block of the .blend file.
    
    Signed-off-by: Nathan Letwory <nathan@mcneel.com>

--- cycles/tools/csycles_shader_exporter.py
+++ cycles/tools/csycles_shader_exporter.py
@@ -22,6 +22,7 @@ allnodes = set()
 alllinks = set()
 
 # Node type to CSycle class name
+# TODO: add all nodes
 nodemapping = {
     'ADD_SHADER' : 'AddClosureNode',
     'MIX_SHADER' : 'MixClosureNode',
@@ -40,6 +41,7 @@ nodemapping = {
     'BSDF_REFRACTION' : 'RefractionBsdfNode',
 }
 
+# Socket type to CSycles class name mapping
 socketmapping = {
     'SHADER' : 'ClosureSocket',
     'VALUE' : 'FloatSocket',
@@ -65,100 +67,29 @@ class Link():
         return '['+ fromnode + ']>' + self.fromsock.name + ' -> ' + \
                 self.tosock.name + '('+self.tosock.type+')<[' + tonode + ']'
     
+    def __repr__(self):
+        return self.__str__()
+    
     def __lt__(a, b):
         return str(a) < str(b)
 
-def find_output_node_of_group(group):
-    for n in group.nodes:
-        if n.type == 'GROUP_OUTPUT':
-            return n
-
-def find_actual_from_link(link, nt_and_parent, depth=0):
-    s = depth*"\t"
-    
-    nt = nt_and_parent[0]
-    parentn = nt_and_parent[1]
-
-    #print(s+"!! got link to " + link.to_node.name + "("+link.to_node.label+"):"+link.to_socket.name + " from " + link.from_node.name +"("+link.from_node.label+"):" + link.from_socket.name)
-    l = link
-
-    # REROUTE node, just check it's input
-    if link.from_node.type =='REROUTE':
-        #print("rerouting")
-        l = find_actual_from_link(link.from_node.inputs[0].links[0], nt_and_parent, depth+1)
-    
-    # GROUP node, find its GROUP_OUTPUT and find corresponding sockets
-    # (names shall be equal)
-    elif link.from_node.type == 'GROUP':
-        ngroup = link.from_node
-        #print("find_actual_from_link: " +s + " >> linking from GROUP " + ngroup.name + "("+ngroup.label+") -- " + ngroup.node_tree.name )
-        outn = find_output_node_of_group(ngroup.node_tree)
-
-        #now find matching sockets (group output socket vs outpnode input socket
-        for inps in outn.inputs:
-
-            # CUSTOM is a point where new sockets can be created in blender interface
-            if inps.type=='CUSTOM':
-                continue
-            if inps.links[0].to_socket.name==link.from_socket.name:
-                #print("find_actual_from_link: " + s + " actual node to link from: " + inps.links[0].from_node.name + "(" + inps.links[0].from_node.label +")")
-                l = find_actual_from_link(inps.links[0], (nt, ngroup.node_tree), depth+1)
-    elif link.from_node.type == 'GROUP_INPUT':
-        for inp in parentn.inputs:
-            if inp.name == link.from_socket.name:
-                l = find_actual_from_link(inp.links[0], nt_and_parent, depth+1) # got a match, lets use that!
-    
-
-    return l
-
-def find_output_node(nodetree):
-    for n in nodetree.nodes:
-        if n.type.startswith('OUTPUT'):
-            return n
-    return None
-
-def find_connections(node_and_tree, depth = 0):
-    node = node_and_tree[0]
-    nt = node_and_tree[1]
-    parentn = node_and_tree[2]
-    for input in node.inputs:
-        if input.is_linked:
-            #shorthand to link
-            lnk = input.links[0]
-            
-            # to and from
-            tonode = lnk.to_node
-            tosocket = lnk.to_socket
-            fromnode = lnk.from_node
-            fromsocket = lnk.from_socket
-            
-            l = None
-            # we need to minimize if any of REROUTE, GROUP_OUTPUT, GROUP_INPUT
-            # or GROUP. Find the node and socket we actually need to link from
-            if fromnode.type in ('REROUTE', 'GROUP_OUTPUT', 'GROUP_INPUT', 'GROUP'):
-                #print("find_connections: need to find actual node, minimizing...")
-                fromlnk = find_actual_from_link(input.links[0], (nt, parentn), depth)
-                l = Link(fromlnk.from_node, fromlnk.from_socket, tonode, tosocket)
-            else: # nothing special, we have the node and socket we need                
-                l = Link(fromnode, fromsocket, tonode, tosocket)
-
-            if l:
-                #print("find_connections: add link", l)
-                alllinks.add(l)
-
-def add_nodes(nodeset, nt, parentn=None):
+def get_node_name(node):
     """
-    Add all useful nodes to a set.
+    Return a cleaned up node name, usable as variable
     """
-    for n in nt.nodes:
-        if not n.type in ('FRAME','REROUTE', 'GROUP_INPUT', 'GROUP_OUTPUT'):
-            if n.type == 'GROUP':
-                add_nodes(nodeset, n.node_tree, n)
-            else:
-                #print("add_nodes: Adding", n.label if n.label else n.name, n.type)
-                nodeset.add((n, nt, parentn))
+    return cleanup_name(node.label if node.label else node.name)
 
-                
+def get_node_name_from_tuple(ntup):
+    return get_node_name(ntup[0])
+
+def cleanup_name(name):
+    """
+    Adapt name so it can be used as a variable name
+    """
+    name = name.replace(" ", "_")
+    name = name.replace(".", "_")
+    return name.lower()
+
 def get_socket_name(socket, socketlist, is_input, node):
     """
     Determine name for a socket. The names given through BPy differ from
@@ -200,6 +131,106 @@ def get_val_string(input):
         )
     return val
 
+def find_output_node_of_group(group):
+    """
+    Get the output node for a group.
+    """
+    for n in group.nodes:
+        if n.type == 'GROUP_OUTPUT':
+            return n
+
+def find_actual_from_link(link, nt_and_parent, depth=0):
+    """
+    Find the node a link is coming from. Simplify by getting rid of
+    REROUTE and group boundaries.
+    """
+    s = depth*"\t"
+    
+    nt = nt_and_parent[0]
+    parentn = nt_and_parent[1]
+
+    l = link
+
+    # REROUTE node, just check it's input
+    if link.from_node.type =='REROUTE':
+        l = find_actual_from_link(link.from_node.inputs[0].links[0], nt_and_parent, depth+1)
+    
+    # GROUP node, find its GROUP_OUTPUT and find corresponding sockets
+    # (names shall be equal)
+    elif link.from_node.type == 'GROUP':
+        ngroup = link.from_node
+        outn = find_output_node_of_group(ngroup.node_tree)
+
+        #now find matching sockets (group output socket vs outpnode input socket
+        for inps in outn.inputs:
+
+            # CUSTOM is a point where new sockets can be created in blender interface
+            if inps.type=='CUSTOM':
+                continue
+            if inps.links[0].to_socket.name==link.from_socket.name:
+                l = find_actual_from_link(inps.links[0], (nt, ngroup.node_tree), depth+1)
+    elif link.from_node.type == 'GROUP_INPUT':
+        for inp in parentn.inputs:
+            if inp.name == link.from_socket.name:
+                l = find_actual_from_link(inp.links[0], nt_and_parent, depth+1) # got a match, lets use that!
+
+    return l
+
+def find_output_node(nodetree):
+    """
+    Get first output node that has incoming links.
+    """
+    for n in nodetree.nodes:
+        if n.type.startswith('OUTPUT'):
+            for inp in n.inputs:
+                if inp.is_linked:
+                    return n
+    return None
+
+def find_connections(node_and_tree, depth = 0):
+    """
+    Iterate over nodes and find the links needed. This process
+    will also minimize the graph by removing redundant nodes like REROUTE
+    and simplify through removing of group boundaries.
+    """
+    node = node_and_tree[0]
+    nt = node_and_tree[1]
+    parentn = node_and_tree[2]
+    for input in node.inputs:
+        if input.is_linked:
+            #shorthand to link
+            lnk = input.links[0]
+            
+            # to and from
+            tonode = lnk.to_node
+            tosocket = lnk.to_socket
+            fromnode = lnk.from_node
+            fromsocket = lnk.from_socket
+            
+            l = None
+            # we need to minimize if any of REROUTE, GROUP_OUTPUT, GROUP_INPUT
+            # or GROUP. Find the node and socket we actually need to link from
+            if fromnode.type in ('REROUTE', 'GROUP_OUTPUT', 'GROUP_INPUT', 'GROUP'):
+                fromlnk = find_actual_from_link(input.links[0], (nt, parentn), depth)
+                l = Link(fromlnk.from_node, fromlnk.from_socket, tonode, tosocket)
+            else: # nothing special, we have the node and socket we need                
+                l = Link(fromnode, fromsocket, tonode, tosocket)
+
+            if l:
+                alllinks.add(l)
+
+def add_nodes(nodeset, nt, parentn=None):
+    """
+    Add all useful nodes to a set.
+    """
+    for n in nt.nodes:
+        if not n.type in ('FRAME','REROUTE', 'GROUP_INPUT', 'GROUP_OUTPUT'):
+            if n.type == 'GROUP':
+                add_nodes(nodeset, n.node_tree, n)
+            else:
+                #print("add_nodes: Adding", n.label if n.label else n.name, n.type)
+                nodeset.add((n, nt, parentn))
+
 def add_inputs(varname, inputs):
     """
     Iterate over given inputs and create input socket value setters
@@ -210,13 +241,13 @@ def add_inputs(varname, inputs):
         if inp.type == 'SHADER': continue
     
         inpname = get_socket_name(inp, inputs, True, inp.node)
-        inputinits = inputinits + "{0}.ins.{1}.Value = {2};\n".format(
+        inputinits = inputinits + "\t{0}.ins.{1}.Value = {2};\n".format(
             varname, inpname, get_val_string(inp)
         )
     
     return inputinits
 
-def initialise_node(node):
+def code_init_node(node):
     """
     Set node variables and sockets to their proper values
     """
@@ -227,30 +258,30 @@ def initialise_node(node):
     # now that inputs have been set, make sure we handle the
     # extra cases, like 'direct member' values as Color and Distribution, Value
     if node.type in ('BSDF_GLOSSY', 'BSDF_REFRACTION'):
-        initcode = initcode + "{0}.Distribution = \"{1}\";\n".format(
+        initcode = initcode + "\t{0}.Distribution = \"{1}\";\n".format(
             varname,
             node.distribution.lower().capitalize()
         )
     if node.type == 'RGB':
-        initcode = initcode + "{0}.Value = new float4({1:.3f}f, {2:.3f}f, {3:.3f}f);\n".format(
+        initcode = initcode + "\t{0}.Value = new float4({1:.3f}f, {2:.3f}f, {3:.3f}f);\n".format(
             varname,
             node.color[0],
             node.color[1],
             node.color[2]
         )
     if node.type == 'VALUE':
-        initcode = initcode + "{0}.Value = {1:.3f}f;\n".format(
+        initcode = initcode + "\t{0}.Value = {1:.3f}f;\n".format(
             varname,
             node.outputs[0].default_value
         )
     if node.type == 'MATH':
-        initcode = initcode + "{0}.Operation = MathNode.Operations.{1};\n".format(
+        initcode = initcode + "\t{0}.Operation = MathNode.Operations.{1};\n".format(
             varname,
             node.operation.lower().capitalize()
         )
     return initcode
 
-def generate_node_code(nodes):
+def code_instantiate_nodes(nodes):
     """
     Construct CSycles nodes for each Blender cycles node,
     initialise and return as one code string.
@@ -260,30 +291,55 @@ def generate_node_code(nodes):
         n = ntup[0]
         if 'OUTPUT' in n.type: continue
            
-        nodeconstruct = "var {0} = new {1}();\n".format(
+        nodeconstruct = "\tvar {0} = new {1}();\n".format(
             get_node_name(n), nodemapping[n.type])
-        nodeinitialise = initialise_node(n)
+        nodeinitialise = code_init_node(n)
         
         code = code + nodeconstruct + nodeinitialise + "\n"
 
     return code
 
-def generate_new_material_shader(shadername):
-    return "var {0} = new Shader(Client, Shader.ShaderType.Material);\n\n{0}.Name = \"{0}\";\n{0}.UseMis = false;\n{0}.UseTransparentShadow = true;\n{0}.HeterogeneousVolume = false;\n\n".format(shadername)
+def code_new_material_shader(shadername):
+    """Start code for a new shader."""
+    return "\tvar {0} = new Shader(Client, Shader.ShaderType.Material);\n\n\t{0}.Name = \"{0}\";\n\t{0}.UseMis = false;\n\t{0}.UseTransparentShadow = true;\n\t{0}.HeterogeneousVolume = false;\n\n".format(shadername)
 
-def add_nodes_to_shader(shadername, nodes):
+def code_nodes_to_shader(shadername, nodes):
+    """Create the code that adds node instances to the shader."""
     addcode = ""
     for ntup in nodes:
         n = ntup[0]
         
         if 'OUTPUT' in n.type: continue
     
-        addcode = addcode + "{0}.Add({1});\n".format(shadername, get_node_name(n))
+        addcode = addcode + "\t{0}.Add({1});\n".format(shadername, get_node_name(n))
     
     return addcode
 
-def generate_linking_code(links):
-    # from -> to: "transparency_color.outs.Color.Connect(eff_trans_col.ins.Color2);"    
+def code_finalise(shadername, links):
+    """Find the links that go into the final output node."""
+    finalisecode = ""
+    for link in links:
+        fromnode = link.fromnode
+        fromsock = link.fromsock
+        tonode = link.tonode
+        tosock = link.tosock
+        
+        if not 'OUTPUT' in tonode.type: continue
+    
+        fromsockname = get_socket_name(fromsock, fromnode.inputs, False, fromnode)
+        tosockname = get_socket_name(tosock, tonode.inputs, True, tonode)
+        
+        finalisecode = finalisecode + "\t{0}.Output.ins.{1}.Connect({2}.outs.{3});\n".format(
+            shadername,
+            tosockname,
+            get_node_name(fromnode),
+            fromsockname
+        )
+    finalisecode = finalisecode + "\n\treturn {0};".format(shadername)
+    return finalisecode
+
+def code_link_nodes(links):
+    """Add code to link nodes to each other."""
     linkcode = ""
     for link in links:
         fromnode = link.fromnode
@@ -291,10 +347,12 @@ def generate_linking_code(links):
         tonode = link.tonode
         tosock = link.tosock
         
+        if 'OUTPUT' in tonode.type: continue
+        
         fromsockname = get_socket_name(fromsock, fromnode.outputs, False, fromnode)
         tosockname = get_socket_name(tosock, tonode.inputs, True, tonode)
         
-        linkcode = linkcode + "{0}.outs.{1}.Connect({2}.ins.{3});\n".format(
+        linkcode = linkcode + "\t{0}.outs.{1}.Connect({2}.ins.{3});\n".format(
             get_node_name(fromnode),
             fromsockname,
             get_node_name(tonode),
@@ -302,12 +360,6 @@ def generate_linking_code(links):
         )
     
     return linkcode        
-
-def get_node_name(node):
-    return node.label if node.label else node.name
-
-def get_node_name_from_tuple(ntup):
-    return get_node_name(ntup[0])
 
 def main():            
     for ob in C.selected_objects:
@@ -318,8 +370,7 @@ def main():
         nt = ob.material_slots[0].material.node_tree
         shadername = ob.material_slots[0].material.name
         
-        shadername = shadername.replace(" ", "_")
-        shadername = shadername.replace(".", "_")
+        shadername = cleanup_name(shadername)
         
         ## seed our nodes set
         add_nodes(allnodes, nt)
@@ -333,19 +384,39 @@ def main():
         nodelist.sort(key=get_node_name_from_tuple)
         linklist = list(alllinks)
         linklist.sort()
+        
+        print(linklist)
 
         # creat new shader
-        shadercode = generate_new_material_shader(shadername)
+        shadercode = code_new_material_shader(shadername)
         # create node setup code        
-        nodesetup = generate_node_code(nodelist)
+        nodesetup = code_instantiate_nodes(nodelist)
         # add our nodes to shader
-        nodeadd = add_nodes_to_shader(shadername, nodelist)
+        nodeadd = code_nodes_to_shader(shadername, nodelist)
         # link our nodes in CSycles
-        linksetup = generate_linking_code(linklist)
+        linksetup = code_link_nodes(linklist)
+        # finalise everything
+        finalisecode = code_finalise(shadername, linklist)
         
-        print(shadercode)
-        print(nodesetup)
-        print(nodeadd)
-        print(linksetup)
+        csycles_shader_creation = "public Shader create_{0}_shader()\n{{\n{1}\n{2}\n{3}\n{4}\n{5}\n}}".format(
+            shadername,
+            shadercode,
+            nodesetup,
+            nodeadd,
+            linksetup,
+            finalisecode
+        )
         
+        exportfile = None
+        exportfilename = "{0}.cs".format(shadername)
+        for t in D.texts:
+            if t.name == exportfilename:
+                exportfile = t
+        
+        if not exportfile:
+            exportfile = D.texts.new(exportfilename)
+        
+        exportfile.clear()
+        exportfile.write(csycles_shader_creation)
+
 main()
