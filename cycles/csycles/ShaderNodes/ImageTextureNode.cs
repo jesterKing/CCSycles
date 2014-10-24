a112dc73d6dda7d57a244265cb7573a942d5456f
Nathan Letwory <nathan@mcneel.com>
2014-10-24 11:37:52 +0300

    Remove irrelevant todo comments.

--- cycles/csycles/ShaderNodes/ImageTextureNode.cs
+++ cycles/csycles/ShaderNodes/ImageTextureNode.cs
@@ -69,7 +69,7 @@ namespace ccl.ShaderNodes
 		public bool IsLinear { get; set; }
 		public bool IsFloat { get; set; }
 		public bool UseAlpha { get; set; }
-		public string Filename { get; set; } /* \todo figure out how to do image loading without using OIIO */
+		public string Filename { get; set; }
 
 		public float[] FloatImage { set; get; }
 
