30eaf63efc826cc126ca7397436a65ac8b50bd94
Nathan Letwory <nathan@mcneel.com>
2015-03-02 11:12:52 +0200

    Add small diagnostics program.

---
+++ ccsycles_diag/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ccl;
+
+namespace ccsycles_diag
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CSycles.set_kernel_path("lib");
+			CSycles.initialise();
+
+			var devices = Device.Devices;
+
+			foreach (var dev in devices)
+			{
+				Console.WriteLine("Name: {0}. Id: {1}. Num: {2}. Description: {3}. DisplayDevice: {4}. Advanced Shading {5}", dev.Name, dev.Id, dev.Num, dev.Description, dev.DisplayDevice, dev.AdvancedShading);
+			}
+			
+			Console.WriteLine("FirstCuda gives us: {0}", Device.FirstCuda);
+
+			CSycles.shutdown();
+		}
+	}
+}
