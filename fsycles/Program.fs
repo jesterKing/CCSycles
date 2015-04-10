e2eb2dead6455fb06b471d96a08960c04e558811
Nathan Letwory <nathan@mcneel.com>
2015-04-11 01:04:42 +0300

    Add a simple F# program
    For now simply initialises CSycles, prints devices and shuts down.

---
+++ fsycles/Program.fs
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open ccl;
+
+let devprint d =
+  printfn "Device %A" d
+
+let print_strings l =
+  for i in l do
+    printfn "%s " i
+
+let print_devices ds =
+  for d in ds do
+    devprint d
+
+[<EntryPoint>]
+let main argv = 
+  print_strings argv
+  printfn ""
+  CSycles.initialise()
+  print_devices Device.Devices
+  CSycles.shutdown()
+  0 // return an integer exit code
