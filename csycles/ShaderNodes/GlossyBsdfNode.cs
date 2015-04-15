fc65007ab9ba7c6eda1287a1a60341b0aa8d75ac
Nathan Letwory <nathan@mcneel.com>
2015-04-15 12:34:42 +0300

    Add constructor with name parameter.
    Stricter class interface.

--- csycles/ShaderNodes/GlossyBsdfNode.cs
+++ csycles/ShaderNodes/GlossyBsdfNode.cs
@@ -24,7 +24,7 @@ namespace ccl.ShaderNodes
 		public FloatSocket Roughness { get; set; }
 		public Float4Socket Normal { get; set; }
 
-		public GlossyInputs(ShaderNode parentNode)
+		internal GlossyInputs(ShaderNode parentNode)
 		{
 			Color = new Float4Socket(parentNode, "Color");
 			AddSocket(Color);
@@ -39,7 +39,7 @@ namespace ccl.ShaderNodes
 	{
 		public ClosureSocket BSDF { get; set; }
 
-		public GlossyOutputs(ShaderNode parentNode)
+		internal GlossyOutputs(ShaderNode parentNode)
 		{
 			BSDF = new ClosureSocket(parentNode, "BSDF");
 			AddSocket(BSDF);
@@ -49,11 +49,12 @@ namespace ccl.ShaderNodes
 	public class GlossyBsdfNode : ShaderNode
 	{
 
-		public GlossyInputs ins { get { return (GlossyInputs)inputs; } set { inputs = value; } }
-		public GlossyOutputs outs { get { return (GlossyOutputs)outputs; } set { outputs = value; } }
+		public GlossyInputs ins { get { return (GlossyInputs)inputs; } }
+		public GlossyOutputs outs { get { return (GlossyOutputs)outputs; } }
 
-		public GlossyBsdfNode() :
-			base(ShaderNodeType.Glossy)
+		public GlossyBsdfNode() : this("a glossy bsdf node") { }
+		public GlossyBsdfNode(string name) :
+			base(ShaderNodeType.Glossy, name)
 		{
 			inputs = new GlossyInputs(this);
 			outputs = new GlossyOutputs(this);
