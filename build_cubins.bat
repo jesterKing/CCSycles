b3027b9ff18a35d2e57571414d28aefe2f3fa9fe
Nathan Letwory <nathan@mcneel.com>
2015-04-18 02:39:15 +0300

    Add quick helper file for me to rebuild cubin files.

---
+++ build_cubins.bat
@@ -0,0 +1,11 @@
+C:\CUDA\bin\nvcc.exe -arch=sm_20 -m64 --cubin D:/Dev/CCSycles/cycles/src/kernel/kernel.cu -o D:/Dev/CCSycles/cycles/src/kernel/kernel_sm_20.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=65 --use_fast_math -ID:/Dev/CCSycles/cycles/src/kernel/../util -ID:/Dev/CCSycles/cycles/src/kernel/svm -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
+
+C:\CUDA\bin\nvcc.exe -arch=sm_21 -m64 --cubin D:/Dev/CCSycles/cycles/src/kernel/kernel.cu -o D:/Dev/CCSycles/lib/kernel_sm_21.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=65 --use_fast_math -ID:/Dev/CCSycles/cycles/src/kernel/../util -ID:/Dev/CCSycles/cycles/src/kernel/svm -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
+
+C:\CUDA\bin\nvcc.exe -arch=sm_30 -m64 --cubin D:/Dev/CCSycles/cycles/src/kernel/kernel.cu -o D:/Dev/CCSycles/lib/kernel_sm_30.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=65 --use_fast_math -ID:/Dev/CCSycles/cycles/src/kernel/../util -ID:/Dev/CCSycles/cycles/src/kernel/svm -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
+
+C:\CUDA\bin\nvcc.exe -arch=sm_35 -m64 --cubin D:/Dev/CCSycles/cycles/src/kernel/kernel.cu -o D:/Dev/CCSycles/lib/kernel_sm_35.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=65 --use_fast_math -ID:/Dev/CCSycles/cycles/src/kernel/../util -ID:/Dev/CCSycles/cycles/src/kernel/svm -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
+
+C:\CUDA\bin\nvcc.exe -arch=sm_50 -m64 --cubin D:/Dev/CCSycles/cycles/src/kernel/kernel.cu -o D:/Dev/CCSycles/lib/kernel_sm_50.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=65 --use_fast_math -ID:/Dev/CCSycles/cycles/src/kernel/../util -ID:/Dev/CCSycles/cycles/src/kernel/svm -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
+
+C:\CUDA\bin\nvcc.exe -arch=sm_52 -m64 --cubin D:/Dev/CCSycles/cycles/src/kernel/kernel.cu -o D:/Dev/CCSycles/lib/kernel_sm_52.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=65 --use_fast_math -ID:/Dev/CCSycles/cycles/src/kernel/../util -ID:/Dev/CCSycles/cycles/src/kernel/svm -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
