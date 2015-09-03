13ec6843a9682a06c3818bce6608ac9ce0346c3a
Nathan Letwory <nathan@mcneel.com>
2015-09-03 10:30:11 +0300

    Add sm_52 to build script

--- build_cubins.bat
+++ build_cubins.bat
@@ -6,7 +6,7 @@ SET shadermodelnum=%1
 SET shadermodel=sm_%shadermodelnum%
 
 IF [%shadermodelnum%]==[] (
-	FOR %%s IN ("sm_20", "sm_21", "sm_30", "sm_35", "sm_50") DO (
+	FOR %%s IN ("sm_20", "sm_21", "sm_30", "sm_35", "sm_50", "sm_52") DO (
 		"%nvcc%" -arch=%%s -m64 --cubin %cyclesroot%/cycles/src/kernel/kernels/cuda/kernel.cu -o %cyclesout%/lib/kernel_%%s.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=%cudaversion% --use_fast_math -I%cyclesroot%/cycles/src/kernel/../util -I%cyclesroot%/cycles/src/kernel/svm -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
 	)
 ) ELSE (
