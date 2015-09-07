e024007c029db6bb7ee6fd01d35de768ceca6597
Nathan Letwory <nathan@mcneel.com>
2015-09-07 09:59:45 +0300

    Add utility class for predefined colors.

---
+++ csycles/Colors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ccl
+{
+	public static class Colors
+	{
+		public static float4 black = new float4(0.0f, 0.0f, 0.0f, 1.0f);
+	}
+}
