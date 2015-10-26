039fa1e65a96f19728d988e92600ea52534813d5
Nathan Letwory <nathan@mcneel.com>
2015-10-26 10:30:50 +0200

    Update the year range.

--- ccycles/ccycles.h
+++ ccycles/ccycles.h
@@ -1,5 +1,5 @@
 /**
-Copyright 2014 Robert McNeel and Associates
+Copyright 2014-2015 Robert McNeel and Associates
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
