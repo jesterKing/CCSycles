3c186b2d3c9c9ecf9ee81c5597879bdfd5736606
Nathan Letwory <nathan@mcneel.com>
2016-02-23 13:21:03 +0200

    Split between CUDA 6.5 and CUDA 7.5
    CUDA 7.5 compiler crashes for sm_20 and sm_21. For now build
    with CUDA 6.5, for as long as we support that in RhinoCycles.

--- build_cubins.bat
+++ build_cubins.bat
@@ -1,14 +1,19 @@
-SET nvcc=C:\CUDA75\bin\nvcc.exe
+SET nvcc=C:\CUDA\bin\nvcc.exe
+SET nvcc75=C:\CUDA75\bin\nvcc.exe
 SET cyclesroot=D:\Dev\Rhino\rhino\src4\rhino4\Plug-ins\RDK\cycles
 SET cyclesout=D:\Dev\Rhino\rhino\big_libs\RhinoCycles
-SET cudaversion=75
+SET cudaversion=65
+SET cudaversion75=75
 SET shadermodelnum=%1
 SET shadermodel=sm_%shadermodelnum%
 
 IF [%shadermodelnum%]==[] (
-	FOR %%s IN ("sm_20", "sm_21", "sm_30", "sm_35", "sm_50", "sm_52") DO (
+	FOR %%s IN ("sm_20", "sm_21") DO (
 		"%nvcc%" -arch=%%s -m64 --cubin %cyclesroot%/cycles/src/kernel/kernels/cuda/kernel.cu -o %cyclesout%/lib/kernel_%%s.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=%cudaversion% --use_fast_math -I%cyclesroot%/cycles/src/kernel/../util -I%cyclesroot%/cycles/src/kernel/svm -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
 	)
+	FOR %%s IN ("sm_30", "sm_35", "sm_50", "sm_52") DO (
+		"%nvcc75%" -arch=%%s -m64 --cubin %cyclesroot%/cycles/src/kernel/kernels/cuda/kernel.cu -o %cyclesout%/lib/kernel_%%s.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=%cudaversion75% --use_fast_math -I%cyclesroot%/cycles/src/kernel/../util -I%cyclesroot%/cycles/src/kernel/svm -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
+	)
 ) ELSE (
-	"%nvcc%" -arch=%shadermodel% -m64 --cubin %cyclesroot%/cycles/src/kernel/kernels/cuda/kernel.cu -o %cyclesout%/lib/kernel_%shadermodel%.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=%cudaversion% --use_fast_math -I%cyclesroot%/cycles/src/kernel/../util -I%cyclesroot%/cycles/src/kernel/svm -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
+	"%nvcc75%" -arch=%shadermodel% -m64 --cubin %cyclesroot%/cycles/src/kernel/kernels/cuda/kernel.cu -o %cyclesout%/lib/kernel_%shadermodel%.cubin --ptxas-options="-v" -D__KERNEL_CUDA_VERSION__=%cudaversion75% --use_fast_math -I%cyclesroot%/cycles/src/kernel/../util -I%cyclesroot%/cycles/src/kernel/svm -DCCL_NAMESPACE_BEGIN= -DCCL_NAMESPACE_END= -DNVCC
 )
